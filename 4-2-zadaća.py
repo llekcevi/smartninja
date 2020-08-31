912eecc6b16dd4bec764130e1c4574e94c6ef17f
llekcevi <69637064+llekcevi@users.noreply.github.com>
2020-08-31 17:19:55 +0200

    Create 4-2-zadaća.py
    Homework 4.2: Add unsuccessful guesses into the dictionary
    
    During the game, collect user's unsuccessful guesses and then store them in the dictionary under the name "wrong_guesses".

---
+++ 4-2-zadaća.py
@@ -0,0 +1,42 @@
+import json
+import random
+import datetime
+
+ime = input("Upiši svoje ime: ")
+
+secret = random.randint(1, 30)
+attempts = 0
+
+with open("score_list.txt", "r") as score_file:
+    score_list = json.loads(score_file.read())    # pretvara string u list i sprema u varijablu score_list
+    print("Top scores: " + str(score_list))
+
+    for score_dict in score_list:
+        print(str(score_dict["attempts"]) + " attempts, name: " + score_dict.get("ime") + ", date: " +
+              score_dict.get("date") + ", secret number: " + str(score_dict["broj"]) + ", wrong guesses: "
+              + str(score_dict.get("wrong")))
+
+wrong = []
+
+while True:
+    guess = int(input("Guess the secret number (between 1 and 30): "))
+    attempts += 1
+
+    if guess == secret:
+        score_list.append({"attempts": attempts, "date": str(datetime.datetime.now()), "ime": ime, "broj": secret,
+                          "wrong": wrong})
+
+        with open("score_list.txt", "w") as score_file:
+            """ otvara .txt kao score file i u njega upisuje list 'score_list' te ga pomoću jsona vraća u string """
+            score_file.write(json.dumps(score_list))
+
+        print("You've guessed it - congratulations! It's number " + str(secret))
+        print("Attempts needed: " + str(attempts))
+        break
+    elif guess > secret:
+        print("Your guess is not correct... try something smaller")
+
+    elif guess < secret:
+        print("Your guess is not correct... try something bigger")
+
+    wrong.append(guess)
